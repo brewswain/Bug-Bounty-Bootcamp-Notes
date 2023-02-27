b4c2c913aad53e6c59584a901eccda9e45370f67
Brandon Lee <brandonlee2543@hotmail.com>
2023-02-27 18:16:37 -0400

    Notes

--- Recon/Scripting/recon.sh
+++ Recon/Scripting/recon.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 DOMAIN=$1
+DATE=$(date +"%Y-%m-%d")
 DIRECTORY=${DOMAIN}_recon
-
+DIRSEARCH_DIRECTORY=$DIRECTORY/dirsearch
+OUTPUT_FILE=${DOMAIN}_${DATE}.txt
 
 echo "Creating directory $DIRECTORY."
-mkdir $DIRECTORY
+mkdir -p $DIRECTORY
+echo "Creating subdirectory $DIRSEARCH_DIRECTORY."
+mkdir -p $DIRSEARCH_DIRECTORY
+touch $DIRSEARCH_DIRECTORY/$OUTPUT_FILE
+echo "Creating Dirsearch Output file: $OUTPUT_FILE"
 nmap $DOMAIN > $DIRECTORY/nmap
 echo "The results of nmap scan are stored in $DIRECTORY/nmap."
-dirsearch -u $DOMAIN -e php -–simple-report=$DIRECTORY/dirsearch
-echo "The results of dirsearch scan are stored in $DIRECTORY/dirsearch."
+dirsearch -u $DOMAIN -e php --output=/home/kali/Desktop/Scripts/Recon/$DIRSEARCH_DIRECTORY/${OUTPUT_FILE} --format=plain
+echo "The results of dirsearch scan are stored in $DIRSEARCH_DIRECTORY."
