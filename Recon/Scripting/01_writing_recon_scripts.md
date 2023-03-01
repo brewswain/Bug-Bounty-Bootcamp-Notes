e66dd7f674bd8debd6d509d5c2df44a18854974a
Brandon Lee <brandonlee2543@hotmail.com>
2023-02-28 22:06:15 -0400

    Notes

--- Recon/Scripting/01_writing_recon_scripts.md
+++ Recon/Scripting/01_writing_recon_scripts.md
@@ -188,4 +188,60 @@ else
 fi
 ```
 
-Nice! We can now add some flags to our script to decide if we want to exclusively only use one specific utility or not.
+Nice! We can now add some flags to our script to decide if we want to exclusively only use one specific utility or not. However,
+we can neaten this up a lil bit by using Case Statement (It's literally just a switch statement lol):
+
+```
+case $TEST_VARIABLE in
+   case1)
+      Do the thing
+      ;;
+   case2)
+      Do the other thing
+      ;;
+   *)
+      Default case here
+      ;;
+esac
+```
+
+Let's implement the statements now!
+
+```
+#!/bin/bash
+
+DOMAIN=$1
+DATE=$(date +"%Y-%m-%d_%H-%M-%S")
+
+DIRSEARCH_DIRECTORY=$DOMAIN/dirsearch
+CRT_DIRECTORY=$DOMAIN/crt
+OUTPUT_FILE=${DOMAIN}_${DATE}.txt
+
+echo "Creating directory $DOMAIN."
+mkdir -p $DOMAIN
+echo "Creating subdirectory $DIRSEARCH_DIRECTORY."
+mkdir -p $DIRSEARCH_DIRECTORY
+touch $DIRSEARCH_DIRECTORY/$OUTPUT_FILE
+echo "Creating Dirsearch Output file: $OUTPUT_FILE"
+
+case $2 in
+   nmap-only)
+      nmap $DOMAIN > $DOMAIN/nmap
+      echo "The results of nmap scan are stored in $DIRECTORY/nmap."
+      ;;
+   dirsearch-only)
+      dirsearch -u $DOMAIN -e php --output=/home/kali/Desktop/Scripts/Recon/$DIRSEARCH_DIRECTORY/${OUTPUT_FILE} --format=plain
+      echo "The results of dirsearch scan are stored in $DIRSEARCH_DIRECTORY."
+      ;;
+   crt-only)
+      ;;
+   *)
+      nmap $DOMAIN > $DOMAIN/nmap
+      echo "The results of nmap scan are stored in $DIRECTORY/nmap."
+      dirsearch -u $DOMAIN -e php --output=/home/kali/Desktop/Scripts/Recon/$DIRSEARCH_DIRECTORY/${OUTPUT_FILE} --format=plain
+      echo "The results of dirsearch scan are stored in $DIRSEARCH_DIRECTORY."
+      curl "https://crt.sh/?q=$DOMAIN&output=json" -o $CRT_DIRECTORY
+      echo "The results of cert parsing is stored in $CRT_DIRECTORY.
+      ;;
+esac
+```
--- Scripts/recon.sh
+++ Scripts/recon.sh
@@ -4,6 +4,7 @@ DOMAIN=$1
 DATE=$(date +"%Y-%m-%d_%H-%M-%S")
 
 DIRSEARCH_DIRECTORY=$DOMAIN/dirsearch
+CRT_DIRECTORY=$DOMAIN/crt
 OUTPUT_FILE=${DOMAIN}_${DATE}.txt
 
 echo "Creating directory $DOMAIN."
@@ -13,17 +14,23 @@ mkdir -p $DIRSEARCH_DIRECTORY
 touch $DIRSEARCH_DIRECTORY/$OUTPUT_FILE
 echo "Creating Dirsearch Output file: $OUTPUT_FILE"
 
-if [ $2 == "nmap-only" ]
-then
-    nmap $DOMAIN > $DOMAIN/nmap
-    echo "The results of nmap scan are stored in $DIRECTORY/nmap."
-elif [ $2 == "dirsearch-only" ]
-then
-    dirsearch -u $DOMAIN -e php --output=/home/kali/Desktop/Scripts/Recon/$DIRSEARCH_DIRECTORY/${OUTPUT_FILE} --format=plain
-    echo "The results of dirsearch scan are stored in $DIRSEARCH_DIRECTORY."
-else
-    nmap $DOMAIN > $DOMAIN/nmap
-    echo "The results of nmap scan are stored in $DIRECTORY/nmap."
-    dirsearch -u $DOMAIN -e php --output=/home/kali/Desktop/Scripts/Recon/$DIRSEARCH_DIRECTORY/${OUTPUT_FILE} --format=plain
-    echo "The results of dirsearch scan are stored in $DIRSEARCH_DIRECTORY."
-fi
+case $2 in
+    nmap-only)
+        nmap $DOMAIN > $DOMAIN/nmap
+        echo "The results of nmap scan are stored in $DIRECTORY/nmap."
+    ;;
+    dirsearch-only)
+        dirsearch -u $DOMAIN -e php --output=/home/kali/Desktop/Scripts/Recon/$DIRSEARCH_DIRECTORY/${OUTPUT_FILE} --format=plain
+        echo "The results of dirsearch scan are stored in $DIRSEARCH_DIRECTORY."
+    ;;
+    crt-only)
+    ;;
+    *)
+        nmap $DOMAIN > $DOMAIN/nmap
+        echo "The results of nmap scan are stored in $DIRECTORY/nmap."
+        dirsearch -u $DOMAIN -e php --output=/home/kali/Desktop/Scripts/Recon/$DIRSEARCH_DIRECTORY/${OUTPUT_FILE} --format=plain
+        echo "The results of dirsearch scan are stored in $DIRSEARCH_DIRECTORY."
+        curl "https://crt.sh/?q=$DOMAIN&output=json" -o $CRT_DIRECTORY
+        echo "The results of cert parsing is stored in $CRT_DIRECTORY."
+    ;;
+esac
